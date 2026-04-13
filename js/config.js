517d381f11ac75eb6a794a161ee7ccccab994071
ColeHarding3 <74082768+ColeHarding3@users.noreply.github.com>
2026-04-13 15:33:16 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // Paste your Google Apps Script Web App URL here after deploying.
   // It will look like: https://script.google.com/macros/s/XXXXXXXXX/exec
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyRpjtP7SClWf_62Y7whF28V03uJNUdx1Qahnr6dqCEq9fOMRRRpCRCW36zLudUr4EN1A/exec",
 
   // Your portfolio display name
   PORTFOLIO_NAME: "My Pokemon Collection",
